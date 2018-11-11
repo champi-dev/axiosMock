b59e4c9b535794e15b555897c53acfbc108a07f9
Daniel Sarmiento Cordero <danielsarcor@gmail.com>
2018-11-11 10:21:50 -0500

    Add utility fn

---
+++ axiosMock.js
@@ -0,0 +1,29 @@
+export default {
+  status: 200,
+  delay: 500,
+  get(route) {
+    return responseSimulator((resolve, reject) => {
+      if (this.status !== 200) reject(new Error())
+
+      switch (route) {
+        case '/transactions':
+          resolve({ data: ['a', 'b', 'c'] })
+          break
+        default:
+          break
+      }
+    }, this.delay)
+  }
+}
+
+const responseSimulator = (callback, delay) => {
+  return new Promise((resolve, reject) => {
+    delaySimulation(callback, delay, resolve, reject)
+  })
+}
+
+const delaySimulation = (callback, delay, resolve, reject) => {
+  setTimeout(() => {
+    callback(resolve, reject)
+  }, delay)
+}
